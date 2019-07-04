b320b0ed86a48c1c41eae20a2192b1849071622a
newnakashima <newnakashima2014@gmail.com>
2019-07-04 16:53:51 +0900

    rm --cached

--- dist/js/script.js
+++
@@ -1,9 +0,0 @@
-var Hoge = /** @class */ (function() {
-  function Hoge() {}
-  Hoge.prototype.fuga = function(str) {
-    return str;
-  };
-  return Hoge;
-})();
-var hoge = new Hoge();
-hoge.fuga("fugafuga");
